5fba2cfe95fe69b78445eb4c0b9a2a4fe52542de
Kaan Celik <kaancelik567@gmail.com>
2025-07-19 22:07:02 +0300

    Updated: inc and dec icon Updated: Settings styles Updated: background worker

--- public/background.js
+++ public/background.js
@@ -1,7 +1,8 @@
-import api from "../src/popup/api/api";
-import { DEC, INC } from "../src/popup/constants";
-import utils from "../src/popup/utils";
-if (utils.isExtensionMode()) {
+const isExtensionMode = () => {
+     return typeof chrome !== 'undefined' && chrome.storage;
+}
+
+if (isExtensionMode()) {
   chrome.alarms.create("checkRates", { periodInMinutes: 60 });
   chrome.alarms.onAlarm.addListener(() => {
     chrome.storage.local.get(
@@ -13,17 +14,27 @@ if (utils.isExtensionMode()) {
   });
 }
 
+const getLatestRates = async () => {
+  const APP_ID = '';
+  const query = `latest?api_key=${APP_ID}`;
+  const baseURL = `https://api.fxfeed.io/v1/`;
+  const response = await fetch(baseURL+query);
+  return await response.json();
+}
+
 const createAlarm = (currency, threshold, rate) => {
   if (!currency || !threshold || !rate) return;
   try {
-    const data = api.getLatestRates();
+    const data = getLatestRates();
     const currentRate = 1 / data.rates[currency];
     const diffPercent = (rate - currentRate) / currentRate;
-    const message = diffPercent < 0 ? DEC : INC;
+    const message = diffPercent < 0 ? 'Decreased by' : 'Increased by';
+    const iconUrl = diffPercent < 0 ? "icons/dec.png" : "icons/inc.png";
     if (Math.abs(diffPercent) > threshold) {
       chrome.notifications.create({
         type: "basic",
         title: "Currency Alert",
+        iconUrl: iconUrl,
         message: `${currency} ${message} ${Math.abs(
           diffPercent
         )}%! Current rate: ${rate.toFixed(2)}`,
